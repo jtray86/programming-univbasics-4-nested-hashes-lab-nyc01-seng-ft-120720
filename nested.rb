a2bc7321923df5cb0517f9f7c5e6b900f31faf9e
Jen Tracy <jenniferetracy@gmail.com>
2020-11-13 19:35:57 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,6 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
+programmer_hash[:alan_kay][:known_for]
 
 end
 
@@ -55,7 +56,7 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -81,7 +82,10 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+programmer_hash[:yukihiro_matsumoto]= {}
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages =>[]}
+programmer_hash[:yukihiro_matsumoto][:languages][0] = "LISP"
+programmer_hash[:yukihiro_matsumoto][:languages][1] = "c"
 end
 
 def changing_alan
