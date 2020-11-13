4601992c1b7f810840369c6553de687bb7953c83
Jen Tracy <jenniferetracy@gmail.com>
2020-11-13 19:36:21 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -84,8 +84,8 @@ def adding_matz
     }
 programmer_hash[:yukihiro_matsumoto]= {}
 programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby",:languages =>[]}
-programmer_hash[:yukihiro_matsumoto][:languages][0] = "LISP"
-programmer_hash[:yukihiro_matsumoto][:languages][1] = "c"
+programmer_hash[:yukihiro_matsumoto][:languages][0] << "LISP"
+programmer_hash[:yukihiro_matsumoto][:languages][1] << "c"
 end
 
 def changing_alan
